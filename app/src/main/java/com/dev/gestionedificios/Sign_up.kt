2048b16b98d60a71bcb80ad70c3dde5897e5b524
Esteban Castañera <ecastanera@frba.utn.edu.ar>
2022-06-24 01:33:53 -0300

    Integracion de registracion con ingreso de datos en BD

--- app/src/main/java/com/dev/gestionedificios/Sign_up.kt
+++ app/src/main/java/com/dev/gestionedificios/Sign_up.kt
@@ -5,9 +5,11 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_auth.*
 import kotlinx.android.synthetic.main.activity_auth.signUpButton
@@ -15,7 +17,10 @@ import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class Sign_up : AppCompatActivity() {
-
+    companion object {
+        var currentUser: UserModel? = null
+    }
+    private val auth = FirebaseAuth.getInstance()
     private  val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,11 +53,12 @@ class Sign_up : AppCompatActivity() {
 
             if (email_EditText.text.isNotEmpty() && password_EditText.text.isNotEmpty() && user_first_name.text.isNotEmpty() && user_lastname.text.isNotEmpty()&& user_phone.text.isNotEmpty())
             {
-                FirebaseAuth.getInstance()
-                    .createUserWithEmailAndPassword(email_EditText.text.toString(),
+                auth.createUserWithEmailAndPassword(email_EditText.text.toString(),
                         password_EditText.text.toString()).addOnCompleteListener {
                         if(it.isSuccessful)
                         {
+
+                            createUserWithoutImage()
                             /*
                             db.collection("user").document("nicolas@gmail.com").set(
                                 hashMapOf(
@@ -82,27 +88,73 @@ class Sign_up : AppCompatActivity() {
 
         }
     }
+    private fun createUserWithoutImage() {
+        val userid = auth.currentUser!!.uid
+        val items = HashMap<String, Any>()
+        items["email"] = ""
+        items["firstName"] = ""
+        items["lastName"] = ""
+        items["userName"] = ""
+        items["phoneNumber"] = ""
+        items["userID"] = userid
+        items["profilePictureURL"] = ""
+        items["active"] = true
+        saveUserToDatabase(auth.currentUser!!, items)
 
-
-    private fun showHome(email:String, provider: ProviderType)
-    {
-
-        val homeIntent= Intent(this,MainActivity::class.java).apply{
-            putExtra("email",email)
-            putExtra("provider",provider.name)
-        }
-
-        startActivity(homeIntent)
     }
+    private fun saveUserToDatabase(user: FirebaseUser, items: HashMap<String, Any>) {
 
-    //Mensaje de alerta si no se puede loguiar
-    private fun showAlert(mensaje:String){
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Error")
-        builder.setMessage(mensaje)
-        builder.setPositiveButton("Aceptar",null)
-        val dialog: AlertDialog = builder.create()
-        dialog.show()
-    }
+        db.collection("users").document(user.uid).set(
+            hashMapOf(
+                "email" to email_EditText.text.toString(),
+                "phone" to user_phone.text.toString(),
+                "firstName" to user_first_name.text.toString(),
+                "lastName" to user_lastname.text.toString(),
+                "profilePictureURL" to "",
+
+            )
+        )
+                                /*.set(items)
+                        .addOnSuccessListener {
+
+
+                   val userModel = UserModel()
+                   userModel.userID = user.uid
+                   userModel.email = items["email"].toString()
+                   userModel.firstName = items["firstName"].toString()
+                   userModel.lastName = items["lastName"].toString()
+                   userModel.userName = items["userName"].toString()
+                   userModel.profilePictureURL = items["profilePictureURL"].toString()
+                   userModel.active = true
+                   currentUser = userModel
+                }.addOnFailureListener {
+                   showAlert("No se registro correctamente")
+
+                    */
+
+
+
+}
+
+private fun showHome(email:String, provider: ProviderType)
+{
+
+val homeIntent= Intent(this,MainActivity::class.java).apply{
+putExtra("email",email)
+putExtra("provider",provider.name)
+}
+
+startActivity(homeIntent)
+}
+
+//Mensaje de alerta si no se puede loguiar
+private fun showAlert(mensaje:String){
+val builder = AlertDialog.Builder(this)
+builder.setTitle("Error")
+builder.setMessage(mensaje)
+builder.setPositiveButton("Aceptar",null)
+val dialog: AlertDialog = builder.create()
+dialog.show()
+}
 
 }
